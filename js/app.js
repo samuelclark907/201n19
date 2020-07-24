8cbf7a3555056d7aa13eb5a3357fd70ae9c0c526
sam <samuelclark907@gmail.com>
2020-07-23 20:20:42 -0800

    completed lab 02

--- js/app.js
+++ js/app.js
@@ -15,11 +15,14 @@ var answerOne = prompt('Do you think I like the rain?');
 var lowerOne = answerOne.toLowerCase();
 console.log(lowerOne);
 if (lowerOne === 'yes' || lowerTwo === 'y') {
-  console.log('Wrong! I do not like the rain')
+  //console.log('Wrong! I do not like the rain')
+  alert('Wrong! I do not like the rain')
 } else if (lowerOne === 'no' || lowerOne === 'n') {
-  console.log('Correct! I do not like the rain')
+  //console.log('Correct! I do not like the rain')
+  alert('Correct! I do not like the rain')
 } else {
-  console.log('Please answer yes or no');
+  //console.log('Please answer yes or no');
+  alert('Please answer yes or no');
 }
 
 
@@ -28,44 +31,56 @@ var answerTwo = prompt('Is chocolate my favorite sweet?')
 var lowerTwo = answerTwo.toLowerCase();
 console.log(lowerTwo);
 if (lowerTwo === 'yes' || lowerTwo === 'y') {
-  console.log('Im sorry thats incorrect, my favorite sweet is Sour Patch Kids')
+  //console.log('Im sorry thats incorrect, my favorite sweet is Sour Patch Kids')
+  alert('Im sorry thats incorrect, my favorite sweet is Sour Patch Kids')
 } else if (lowerTwo === 'no' || lowerTwo === 'n') {
-  console.log('Correct my favorite sweet is Sour Patch Kids.')
+  //console.log('Correct my favorite sweet is Sour Patch Kids.')
+  alert('Correct my favorite sweet is Sour Patch Kids.')
 } else {
-  console.log('Please answer yes or no');
+  //console.log('Please answer yes or no');
+  alert('Please answer yes or no');
 }
 
 var answerThree = prompt('Do I like to drive fast?');
 var lowerThree = answerThree.toLowerCase();
 console.log(lowerThree);
 if (lowerThree === 'yes' || lowerThree === 'y') {
-  console.log('Correct I love driving fast')
+  //console.log('Correct I love driving fast')
+  alert('Correct I love driving fast')
 } else if (lowerThree === 'no' || lowerThree === 'n') {
-  console.log('Wrong driving slow is for old people.')
+  //console.log('Wrong driving slow is for old people.')
+  alert('Wrong driving slow is for old people.')
 } else {
-  console.log('Please answer yes or no');
+  //console.log('Please answer yes or no');
+  alert('Please answer yes or no');
 }
 
 var answerFour = prompt('Is my favorite color blue?');
 var lowerFour = answerFour.toLowerCase();
 console.log(lowerFour);
 if (lowerFour === 'yes' || lowerFour === 'y') {
-  console.log('Wrong my favorite color is red')
+  //console.log('Wrong my favorite color is red')
+  alert('Wrong my favorite color is red')
 } else if (lowerFour === 'no' || lowerFour === 'n') {
-  console.log('Correct I love the color red')
+  //console.log('Correct I love the color red')
+  alert('Correct I love the color red')
 } else {
-  console.log('Please answer yes or no');
+  //console.log('Please answer yes or no');
+  alert('Please answer yes or no');
 }
 
 var answerFive = prompt('Am I a vegitarian?');
 var lowerFive = answerFive.toLowerCase();
 console.log(lowerFive);
 if (lowerFive === 'yes' || lowerFive === 'y') {
-  console.log('Wrong I love me some bacon')
+  //console.log('Wrong I love me some bacon')
+  alert('Wrong I love me some bacon')
 } else if (lowerFive === 'no' || lowerFive === 'n') {
-  console.log('Correct I love to eat all meat.')
+  //console.log('Correct I love to eat all meat.')
+  ('Correct I love to eat all meat.')
 } else {
-  console.log('Please answer yes or no');
+  //console.log('Please answer yes or no');
+  alert('Please answer yes or no');
 }
 
 alert('Thank you ' + name + ' for playing who wants to be a millionaire');
